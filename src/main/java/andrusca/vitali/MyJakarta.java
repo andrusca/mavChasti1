4e45d86a570d8473d6c1e5c7a1fcbeedeffeb60a
vitalie <vitalie.v.andrusca@gmail.com>
2026-05-22 13:46:17 +0300

    Settted up data for classes.

---
+++ src/main/java/andrusca/vitali/MyJakarta.java
@@ -0,0 +1,22 @@
+package andrusca.vitali;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Class for
+ *
+ * @author Vitalie
+ * <p>
+ * on 22.05.2026
+ */
+
+@Data
+public class MyJakarta {
+    private long version;
+    private String description;
+    private List <Technology> techList;
+
+
+}
---
+++ src/main/java/andrusca/vitali/Technology.java
@@ -0,0 +1,19 @@
+package andrusca.vitali;
+
+import lombok.Data;
+
+/**
+ * Class for
+ *
+ * @author Vitalie
+ * <p>
+ * on 22.05.2026
+ */
+
+@Data
+public class Technology {
+
+    private String techName;
+    private String  techDescription;
+
+}
