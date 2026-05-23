522f5a07365f3f8daf4db294a1a1fae5df0f8ad2
vitalie <vitalie.v.andrusca@gmail.com>
2026-05-23 11:01:59 +0300

    Ran from main class writeToJson method

--- src/main/java/andrusca/vitali/MyJakarta.java
+++ src/main/java/andrusca/vitali/MyJakarta.java
@@ -3,7 +3,10 @@ package andrusca.vitali;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
@@ -13,7 +16,7 @@ import java.util.List;
  * <p>
  * on 22.05.2026
  */
-
+@Slf4j
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
@@ -22,6 +25,16 @@ public class MyJakarta {
     private long version;
     private String description;
     private List <Technology> techList;
+    private static final String  pathFileStructure = "output.txt";
+
+    public static void main(String[] args) {
+        Technology technology = new Technology("real technology", "developing stuff");
+        try {
+            technology.writeToJson(pathFileStructure);
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
+    }
 
 
 }
